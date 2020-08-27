c0b4c27956a8a82bdc338d496962282c809c597f
KelahKelah <kelechiekebor@gmail.com>
2020-08-27 11:06:37 +0100

    add a loader

---
+++ src/components/loader/loader.js
@@ -0,0 +1,12 @@
+import React from "react";
+import "./loader.css";
+
+const Loader = () =>{
+    return (
+      <div id="small-overlay">
+        <div className="small-loader"></div>
+      </div>
+    );
+}
+
+export default Loader;
