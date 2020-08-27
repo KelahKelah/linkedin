49749823f0ecc48567ef3cac54c913ef48013801
KelahKelah <kelechiekebor@gmail.com>
2020-08-27 11:05:15 +0100

    fetch comments from endpoint for display

--- src/home/home.js
+++ src/home/home.js
@@ -1,69 +1,82 @@
 import React, { useState, useEffect } from "react";
 import styles from "./home.module.css";
 import Img from "../assets/profile.jpeg";
-import {BsLink } from 'react-icons/bs';
+import { BsLink } from "react-icons/bs";
 import Axios from "axios";
-import UserId  from '../components/userId/userId';
+import UserId from "../components/userId/userId";
+import Loader from "../components/loader/loader";
 
-
-const url = "https://dummyapi.io/data/api/user?"
-const appId = "5f463d5fdc92550002496c7c"
+const url = "https://dummyapi.io/data/api/user?";
+const appId = "5f463d5fdc92550002496c7c";
 
 const Home = (props) => {
-    const [userData, setUserData] = useState([]);
-    const [userId, setUserId] = useState('');
+  const [userData, setUserData] = useState([]);
+  const [userId, setUserId] = useState("");
+  const [loader, setLoader] = useState(false);
 
-    useEffect(()=> {
-        Axios.get("https://dummyapi.io/data/api/user?", {
-            headers: {
-                'app-id': `${appId}`
-            }
-        })
-        .then(res => {
-            console.log('The response is',res)
-            if(res.status === 200) {
-                setUserData(res.data.data)
-                console.log('user data is', userId)
+  useEffect(() => {
+    Axios.get("https://dummyapi.io/data/api/user?", {
+      headers: {
+        "app-id": `${appId}`,
+      },
+    }).then((res) => {
+      // console.log('The response is',res)
+      if (res.status === 200) {
+        setUserData(res.data.data);
+        console.log("user data is", userData);
+        setLoader(true);
+      }
+    });
+  }, []);
 
-            }
-        });
+  const handleClick = (userId) => {
+    // setUserId(userData.id)
+    props.history.push(`/user/${userId}`);
+    // console.log('UserId is ', userData.id)
+  };
 
-    }, [])
-
-    const handleClick = () => {
-        // setUserId(userData.id)
-        props.history.push('/user/userId')
-        // console.log('UserId is ', userData.id)
-    }
-
-  
-  return (
-      <div className={`container ${styles.homeContainer}`}>
-                        <div className={`row ${styles.homeRow}`}>
-
-          {userData.length > 0 && userData.map((user)=> {
-              return(
-                  <div className={"col-md-3"}>
-                      <div className={styles.homeCol}>
-                        {/* <button type="button"  aria-label="Close"> */}
-                            <span aria-hidden="true" className={`close ${styles.homeClose}`}>&times;</span>
-                        {/* </button> */}
-                        <img src={user.picture} alt="" />
-                        <h4>{`${user.firstName} ${user.lastName}`}</h4>
-                        <p>{user.email}</p>
-                        <p className={styles.subItem}><BsLink />60 mutual connections</p>
-                        <button onClick={handleClick}>View</button>
-                      </div>
-                 </div>
-          )})}
-          </div>
+  return !loader ? (
+    <Loader />
+  ) : (
+    <div className={`container ${styles.homeContainer}`}>
+      <h6>People you may know</h6>
+      <div className={`row ${styles.homeRow}`}>
+        {userData.length > 0 &&
+          userData.map((user, index) => {
+            return (
+              <div className={"col-md-3"}>
+                <div className={styles.homeCol}>
+                  {/* <button type="button"  aria-label="Close"> */}
+                  <span
+                    aria-hidden="true"
+                    className={`close ${styles.homeClose}`}
+                  >
+                    &times;
+                  </span>
+                  {/* </button> */}
+                  <img src={user.picture} alt="" />
+                  <h4
+                    className={styles.name}
+                  >{`${user.firstName} ${user.lastName}`}</h4>
+                  <p className={styles.email}>{user.email}</p>
+                  <p className={styles.subItem}>
+                    <BsLink />
+                    60 mutual connections
+                  </p>
+                  <button
+                    onClick={() => {
+                      handleClick(user.id);
+                    }}
+                  >
+                    View
+                  </button>
+                </div>
+              </div>
+            );
+          })}
       </div>
-      
+    </div>
   );
-
-//   const UserIdModal = () => {
-      
-//   }
 
 };
 export default Home;
