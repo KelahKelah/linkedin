de11094b81a09cae019ade78fd98ddbc749d1aa9
KelahKelah <kelechiekebor@gmail.com>
2020-08-31 09:53:33 +0100

    added header

--- src/App.js
+++ src/App.js
@@ -5,14 +5,16 @@ import UserId from './components/userId/userId';
 import {BrowserRouter, Switch, Route} from 'react-router-dom';
 import SingleCard from './components/singleCard/singleCard';
 import SingleCardUser from './components/singleCardUser/singleCardUser';
+import Header from './components/header/header';
 
 function App() {
   return (
     <div>
+      <Header />
       <BrowserRouter>
         <Switch>
           <Route exact path="/use/:id" component={SingleCardUser} />
-          <Route exact path='/' component={Home} />
+          <Route exact path='/'><Home /></Route>
           <Route exact path='/user/:id' component={UserId} />
           <Route exact path='/single' component={SingleCard} />
         </Switch>
